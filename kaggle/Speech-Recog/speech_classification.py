c42bcc61d3209f6757b958411a7ae3347e2907f6
ghostian <junxiao.guo@outlook.com>
2019-09-23 21:06:03 -0400

    edit folder

--- kaggle/Speech-Recog/speech_classification.py
+++ kaggle/Speech-Recog/speech_classification.py
@@ -7,6 +7,8 @@ import torch.nn.functional as F
 import torch.optim as optim
 from torch.autograd import Variable
 from torch.utils import data
+import time
+
 cuda = torch.cuda.is_available()
 
 
@@ -83,38 +85,44 @@ class Trainer():
     def save_model(self, path):
         torch.save(self.model.state_dict(), path)
 
-    def run(self, nepochs, x, y):
-        train_dataset = SquaredDataset(x, y)
-        train_loader_args = dict(shuffle=True, batch_size=256, num_workers=0, pin_memory=True) if cuda \
-            else dict(shuffle=True, batch_size=64)
-        train_loader = data.DataLoader(train_dataset, **train_loader_args)
-        for epoch in range(nepochs):
-            # print("Epoch", epoch)
-            model.train()
-            model.to(device)
-            running_loss = 0.0
-            scheduler.step()
-            correct = 0
-            for batch_idx, (x, y) in enumerate(train_loader):
-                self.optimizer.zero_grad()
-                X = Variable(x.float()).to(device)
-                Y = Variable(y).to(device)
-                out = model(X)
-                pred = out.data.max(1, keepdim=True)[1]
-                predicted = pred.eq(Y.data.view_as(pred))
-                correct += predicted.sum()
-                loss = F.nll_loss(out, Y)
-                running_loss += loss.item()
-                loss.backward()
-                self.optimizer.step()
-            if (epoch + 1) % 20 == 0:
-                print("epoch:{}   loss:{} correct:{} out of {}".format(epoch + 1, loss, correct, 64*len(train_loader)))
+    def train_per_epoch(self, x, y):
+        # train_dataset = SquaredDataset(x, y)
+        # train_loader_args = dict(shuffle=True, batch_size=256, num_workers=0, pin_memory=True) if cuda \
+        #     else dict(shuffle=True, batch_size=64)
+        # train_loader = data.DataLoader(train_dataset, **train_loader_args)
+        # for epoch in range(nepochs):
+        # print("Epoch", epoch)
+        model.train()
+        model.to(device)
+        running_loss = 0.0
+        # scheduler.step()
+        correct = 0
+        samples = 0
+        for batch_idx, (x, y) in enumerate(train_loader):
+            self.optimizer.zero_grad()
+            X = Variable(x.float()).to(device)
+            Y = Variable(y).to(device)
+            if len(X) == 1:
+                continue
+            out = model(X)
+            pred = out.data.max(1, keepdim=True)[1]
+            predicted = pred.eq(Y.data.view_as(pred))
+            correct += predicted.sum()
+            samples += len(y)
+            loss = F.nll_loss(out, Y)
+            running_loss += loss.item()
+            loss.backward()
+            self.optimizer.step()
+            # if (epoch + 1) % 20 == 0:
+        # print("loss:{} corret:{} @ {}".format(loss,correct,256*len(train_loader)))
+        running_loss /= len(train_loader)
+        return correct, samples, running_loss
 
 
 def inference(model, loader, n_members):
     correct = 0
     for data, label in loader:
-        X = Variable(data.view(-1, 40))
+        X = Variable(data)
         Y = Variable(label)
         out = model(X)
         pred = out.data.max(1, keepdim=True)[1]
@@ -124,8 +132,11 @@ def inference(model, loader, n_members):
 
 
 if __name__ == "__main__":
-    trainy = np.load("source_data.nosync/train_labels.npy", allow_pickle=True)[:200]
-    trainx = np.load("source_data.nosync/train.npy", allow_pickle=True)[:200]
+    print("Cuda:{}".format(cuda))
+    # trainx = np.load("source_data.nosync/train.npy", allow_pickle=True)[:200]
+    # trainy = np.load("source_data.nosync/train_labels.npy", allow_pickle=True)[:200]
+    trainy = np.load("train_labels.npy", allow_pickle=True)
+    trainx = np.load("train.npy", allow_pickle=True)
     mydata = MyDataset(X=trainx, Y=trainy)
     device = torch.device("cuda" if cuda else "cpu")
     model = Pred_Model()
@@ -133,9 +144,27 @@ if __name__ == "__main__":
     optimizer = optim.SGD(model.parameters(), lr=0.03)
     scheduler = torch.optim.lr_scheduler.StepLR(optimizer, step_size=4, gamma=0.1)
     trainer = Trainer(model, optimizer)
-    for i in range(1, len(mydata.X)):
-        for cur_x, cur_y in zip(mydata.X[i - 1:i + 1], mydata.Y[i - 1:i + 1]):
-            trainer.run(nepochs=80, x=cur_x, y=cur_y)
+    nepoch = 10
+    tot_correct = 0
+    tot_samples = 0
+    for epoch in range(nepoch):
+        print("epoch:{}".format(epoch + 1))
+        tot_correct = 0
+        tot_samples = 0
+        start_time = time.time()
+        for cur_x, cur_y in zip(mydata.X, mydata.Y):
+            train_dataset = SquaredDataset(cur_x, cur_y)
+            train_loader_args = dict(shuffle=True, batch_size=256, num_workers=0, pin_memory=True) if cuda \
+                else dict(shuffle=True, batch_size=256)
+            train_loader = data.DataLoader(train_dataset, **train_loader_args)
+            a, b, c = trainer.train_per_epoch(x=cur_x, y=cur_y)
+            tot_correct += a
+            tot_samples += b
+            # for i in range(1, len(mydata.X)):
+            #     for cur_x, cur_y in zip(mydata.X[i - 1:i + 1], mydata.Y[i - 1:i + 1]):
+            #         trainer.train_per_epoch(nepochs=80, x=cur_x, y=cur_y)
+        end_time = time.time()
+        print("Loss: {}   Correct: {}  Samples: {} Accuracy:{}  Time: {}".format(c, tot_correct, tot_samples, float(tot_correct / tot_samples), end_time - start_time))
     trainer.save_model('./saved_model.pt')
     print("testing")
 
