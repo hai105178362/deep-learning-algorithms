66a9f0bab6f07f379069e16c369120d213635740
ghostian <junxiao.guo@outlook.com>
2019-10-12 14:38:04 -0400

    CNN update

--- kaggle/CNN/cnn.py
+++ kaggle/CNN/cnn.py
@@ -1,109 +1,205 @@
-import torch
-import torch.nn.functional as F
-from torch import autograd, nn
-from torch.autograd import Variable
+import os
 import numpy as np
-import matplotlib.pyplot as plt
-from torchvision import transforms, datasets
-from skimage import io
+from PIL import Image
+import torch
+import torchvision
+import torch.nn as nn
+import torch.nn.functional as F
+from torch.utils.data import Dataset, DataLoader
 
-#
-# input_image = autograd.Variable(torch.randn(1,3,32,32)) # single 32x32 RGB image
-# print(input_image.size())
-# input_signal = autograd.Variable(torch.randn(1,40,100)) # 40 dimensional signal for 100 timesteps
-# print(input_signal.size())
-#
-# # Create layers
-# layer_c2d = torch.nn.Conv2d(in_channels=3, out_channels=20, kernel_size=5, stride=1, padding=2)
-# layer_avg = torch.nn.AvgPool2d(kernel_size=32)
-#
-# # Use layers directly
-# y = layer_c2d(input_image)
-# print(y.size())
-#
-# # Add layers to model
-# model = torch.nn.Sequential(layer_avg, layer_c2d)
-# y = model(input_image)
-# print(y.size())
-#
-# filters = autograd.Variable(torch.randn(20,3,5,5)) # 5x5 filter from 3 dimensions to 20
-# y=F.conv2d(input_image, filters, padding=2)
-# print(y.size())
-#
-# filters = autograd.Variable(torch.randn(256,40,5)) # 5 wide filter from 40 dimensions to 256
-# y=F.conv1d(input_signal, filters, padding=2)
-# print(y.size())
-#
-# ####################READ DATA#######################
-# image = io.imread('data.nosync/11785-f19-hw2p2-classification/11-785hw2p2-f19/train_data/medium/0/0002_01.jpg')
-# print(image.shape)
-# # Display an image
-# plt.imshow(image)
-# plt.show()
+class ImageDataset(Dataset):
+    def __init__(self, file_list, target_list):
+        self.file_list = file_list
+        self.target_list = target_list
+        self.n_class = len(list(set(target_list)))
 
+    def __len__(self):
+        return len(self.file_list)
 
-# Use torch ImageFolder
-# data_transform = transforms.Compose([
-#         transforms.RandomResizedCrop(100),
-#         transforms.RandomHorizontalFlip(),
-#         transforms.ToTensor()
-#     ])
-# face_dataset = datasets.ImageFolder(root='data.nosync/11785-f19-hw2p2-classification/11-785hw2p2-f19/train_data/medium',
-#                                            transform=data_transform)
-# dataset_loader = torch.utils.data.DataLoader(face_dataset,
-#                                              batch_size=2,
-#                                              shuffle=True,
-#                                              num_workers=1)
-# for x,y in dataset_loader:
-#     x = x.numpy()
-#     print(x.shape)
-#     for img, label in zip(x,y):
-#         print("Label: {}".format(label))
-#         plt.imshow(img.transpose((1,2,0)))
-#         plt.show()
+    def __getitem__(self, index):
+        img = Image.open(self.file_list[index])
+        img = torchvision.transforms.ToTensor()(img)
+        label = self.target_list[index]
+        return img, label
 
+def parse_data(datadir):
+    img_list = []
+    ID_list = []
+    for root, directories, filenames in os.walk(datadir):
+        for filename in filenames:
+            if filename.endswith('.jpg'):
+                filei = os.path.join(root, filename)
+                img_list.append(filei)
+                ID_list.append(root.split('/')[-1])
 
-class BasicCNNModule(nn.Module):
-    def __init__(self):
-        super(BasicCNNModule, self).__init__()
-        self.conv1 = nn.Conv2d(3, 6, 5)
-        self.pool = nn.MaxPool2d(2, 2)
-        self.conv2 = nn.Conv2d(6, 16, 5)
-        self.fc1 = nn.Linear(16 * 5 * 5, 120)
-        self.fc2 = nn.Linear(120, 84)
-        self.fc3 = nn.Linear(84, 10)
+    # construct a dictionary, where key and value correspond to ID and target
+    uniqueID_list = list(set(ID_list))
+    class_n = len(uniqueID_list)
+    target_dict = dict(zip(uniqueID_list, range(class_n)))
+    label_list = [target_dict[ID_key] for ID_key in ID_list]
+
+    print('{}\t\t{}\n{}\t\t{}'.format('#Images', '#Labels', len(img_list), len(set(label_list))))
+    return img_list, label_list, class_n
+
+class BasicBlock(nn.Module):
+
+    def __init__(self, channel_size, stride=1):
+        super(BasicBlock, self).__init__()
+        self.conv1 = nn.Conv2d(channel_size, channel_size, kernel_size=3, stride=stride, padding=1, bias=False)
+        self.bn1 = nn.BatchNorm2d(channel_size)
+        self.shortcut = nn.Conv2d(channel_size, channel_size, kernel_size=1, stride=stride, bias=False)
 
     def forward(self, x):
-        x = self.pool(Relu(self.conv1(x)))
-        x = self.pool(Relu(self.conv2(x)))
-        x = x.view(-1, 16 * 5 * 5)
-        x = Relu(self.fc1(x))
-        x = Relu(self.fc2(x))
-        x = self.fc3(x)
-        return x
+        out = F.relu(self.bn1(self.conv1(x)))
+        out = self.bn1(self.conv1(out))
+        out += self.shortcut(x)
+        out = F.relu(out)
+        return out
+
+class Network(nn.Module):
+    def __init__(self, num_feats, hidden_sizes, num_classes, feat_dim=10):
+        super(Network, self).__init__()
+
+        self.hidden_sizes = [num_feats] + hidden_sizes + [num_classes]
+
+        self.layers = []
+        for idx, channel_size in enumerate(hidden_sizes):
+            self.layers.append(nn.Conv2d(in_channels=self.hidden_sizes[idx],
+                                         out_channels=self.hidden_sizes[idx + 1],
+                                         kernel_size=3, stride=2, bias=False))
+            self.layers.append(nn.ReLU(inplace=True))
+            self.layers.append(BasicBlock(channel_size=channel_size))
+
+        self.layers = nn.Sequential(*self.layers)
+        self.linear_label = nn.Linear(self.hidden_sizes[-2], self.hidden_sizes[-1], bias=False)
+
+        # For creating the embedding to be passed into the Center Loss criterion
+        self.linear_closs = nn.Linear(self.hidden_sizes[-2], feat_dim, bias=False)
+        self.relu_closs = nn.ReLU(inplace=True)
+
+    def forward(self, x, evalMode=False):
+        output = x
+        output = self.layers(output)
+
+        output = F.avg_pool2d(output, [output.size(2), output.size(3)], stride=1)
+        output = output.reshape(output.shape[0], output.shape[1])
+
+        label_output = self.linear_label(output)
+        label_output = label_output / torch.norm(self.linear_label.weight, dim=1)
+
+        # Create the feature embedding for the Center Loss
+        closs_output = self.linear_closs(output)
+        closs_output = self.relu_closs(closs_output)
+
+        return closs_output, label_output
 
 
-# print(BasicCNNModule())
-
-args,kwargs = [], {}
-model = BasicCNNModule()
-# save only model parameters
-PATH = "./basic_cnn.pt"
-torch.save(model.state_dict(), PATH)
-
-# load a saved model parameters
-model = BasicCNNModule()
-model.load_state_dict(torch.load(PATH))
-
-## Less optimised approaches ->
-# saving the entire model
-torch.save(model, PATH)
-
-# load the saved model
+def init_weights(m):
+    if type(m) == nn.Conv2d or type(m) == nn.Linear:
+        torch.nn.init.xavier_normal_(m.weight.data)
 
 
-import torchvision.models as models
+def train(model, data_loader, test_loader, task='Classification'):
+    model.train()
 
-vgg16 = models.vgg16(pretrained=True) # this might take a while
-vgg16.eval()
-print(vgg16)
+    for epoch in range(numEpochs):
+        avg_loss = 0.0
+        for batch_num, (feats, labels) in enumerate(data_loader):
+            feats, labels = feats.to(device), labels.to(device)
+
+            optimizer.zero_grad()
+            outputs = model(feats)[1]
+
+            loss = criterion(outputs, labels.long())
+            loss.backward()
+            optimizer.step()
+
+            avg_loss += loss.item()
+
+            if batch_num % 50 == 49:
+                print('Epoch: {}\tBatch: {}\tAvg-Loss: {:.4f}'.format(epoch + 1, batch_num + 1, avg_loss / 50))
+                avg_loss = 0.0
+
+            torch.cuda.empty_cache()
+            del feats
+            del labels
+            del loss
+
+        if task == 'Classification':
+            val_loss, val_acc = test_classify(model, test_loader)
+            train_loss, train_acc = test_classify(model, data_loader)
+            print('Train Loss: {:.4f}\tTrain Accuracy: {:.4f}\tVal Loss: {:.4f}\tVal Accuracy: {:.4f}'.
+                  format(train_loss, train_acc, val_loss, val_acc))
+        else:
+            test_verify(model, test_loader)
+
+
+def test_classify(model, test_loader):
+    model.eval()
+    test_loss = []
+    accuracy = 0
+    total = 0
+
+    for batch_num, (feats, labels) in enumerate(test_loader):
+        feats, labels = feats.to(device), labels.to(device)
+        outputs = model(feats)[1]
+
+        _, pred_labels = torch.max(F.softmax(outputs, dim=1), 1)
+        pred_labels = pred_labels.view(-1)
+
+        loss = criterion(outputs, labels.long())
+
+        accuracy += torch.sum(torch.eq(pred_labels, labels)).item()
+        total += len(labels)
+        test_loss.extend([loss.item()] * feats.size()[0])
+        del feats
+        del labels
+
+    model.train()
+    return np.mean(test_loss), accuracy / total
+
+
+def test_verify(model, test_loader):
+    raise NotImplementedError
+
+
+if __name__ == '__main__':
+    device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
+    print("device: ",device)
+
+    img_list, label_list, class_n = parse_data('data.nosync/11785-f19-hw2p2-classification/11-785hw2p2-f19/train_data/medium')
+    trainset = ImageDataset(img_list, label_list)
+    train_data_item, train_data_label = trainset.__getitem__(0)
+    print('data item shape: {}\t data item label: {}'.format(train_data_item.shape, train_data_label))
+    dataloader = DataLoader(trainset, batch_size=10, shuffle=True, num_workers=1, drop_last=False)
+    imageFolder_dataset = torchvision.datasets.ImageFolder(root='data.nosync/11785-f19-hw2p2-classification/11-785hw2p2-f19/train_data/medium/',
+                                                           transform=torchvision.transforms.ToTensor())
+    imageFolder_dataloader = DataLoader(imageFolder_dataset, batch_size=10, shuffle=True, num_workers=1)
+    print(imageFolder_dataset.__len__(), len(imageFolder_dataset.classes))
+
+    train_dataset = torchvision.datasets.ImageFolder(root='data.nosync/11785-f19-hw2p2-classification/11-785hw2p2-f19/train_data/medium/',
+                                                     transform=torchvision.transforms.ToTensor())
+    train_dataloader = torch.utils.data.DataLoader(train_dataset, batch_size=10,
+                                                   shuffle=True, num_workers=8)
+
+    dev_dataset = torchvision.datasets.ImageFolder(root='data.nosync/11785-f19-hw2p2-classification/11-785hw2p2-f19/validation_classification/medium/',
+                                                   transform=torchvision.transforms.ToTensor())
+    dev_dataloader = torch.utils.data.DataLoader(dev_dataset, batch_size=10,
+                                                 shuffle=True, num_workers=8)
+    numEpochs = 4
+    num_feats = 3
+
+    learningRate = 1e-2
+    weightDecay = 5e-5
+
+    hidden_sizes = [32, 64]
+    num_classes = len(train_dataset.classes)
+
+    network = Network(num_feats, hidden_sizes, num_classes)
+    network.apply(init_weights)
+
+    criterion = nn.CrossEntropyLoss()
+    optimizer = torch.optim.SGD(network.parameters(), lr=learningRate, weight_decay=weightDecay, momentum=0.9)
+    network.train()
+    network.to(device)
+    train(network, train_dataloader, dev_dataloader)
