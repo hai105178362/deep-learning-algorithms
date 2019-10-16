469e38514ad9427ec3a2c822119490548c84236f
ghostian <junxiao.guo@outlook.com>
2019-10-16 18:29:07 -0400

    dev

--- kaggle/CNN/cnn_params.py
+++ kaggle/CNN/cnn_params.py
@@ -23,9 +23,10 @@ allspec = "closs_weight:{}\t".format(closs_weight) + "lr_cent:{}\t".format(lr_ce
           "hidden_size:{}".format(hidden_sizes)
 
 device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
-train_dataset = torchvision.datasets.ImageFolder(root='data.nosync/validation_classification/medium', transform=torchvision.transforms.ToTensor())
+# train_dataset = torchvision.datasets.ImageFolder(root='data.nosync/validation_classification/medium', transform=torchvision.transforms.ToTensor())
+train_dataset = torchvision.datasets.ImageFolder(root='dataset/train_data/medium', transform=torchvision.transforms.ToTensor())
 # train_dataset = torchvision.datasets.ImageFolder(root='devset/medium', transform=torchvision.transforms.ToTensor())
-dev_dataset = torchvision.datasets.ImageFolder(root='data.nosync/validation_classification/medium', transform=torchvision.transforms.ToTensor())
+dev_dataset = torchvision.datasets.ImageFolder(root='dataset/validation_classification/medium', transform=torchvision.transforms.ToTensor())
 # dev_dataset = torchvision.datasets.ImageFolder(root='devset/medium_dev', transform=torchvision.transforms.ToTensor())
 train_dataloader = torch.utils.data.DataLoader(train_dataset, batch_size=128, shuffle=True, num_workers=8)
 dev_dataloader = torch.utils.data.DataLoader(dev_dataset, batch_size=128, shuffle=True, num_workers=8)
