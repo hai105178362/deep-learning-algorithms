be94ae182ee40149172c8951b5bc6d7c7851f1c1
ghostian <junxiao.guo@outlook.com>
2019-10-21 15:26:58 -0400

    HW3P1 init

--- Non_Lib/hw3/gru.py
+++ Non_Lib/hw3/gru.py
@@ -2,30 +2,38 @@ import numpy as np
 
 HIDDEN_DIM = 4
 
+
 class Sigmoid:
     # DO NOT DELETE
-	def __init__(self):
-		pass
-	def forward(self, x):
-		self.res = 1/(1+np.exp(-x))
-		return self.res
-	def backward(self):
-		return self.res * (1-self.res)
-	def __call__(self, x):
-		return self.forward(x)
+    def __init__(self):
+        pass
+
+    def forward(self, x):
+        self.res = 1 / (1 + np.exp(-x))
+        return self.res
+
+    def backward(self):
+        return self.res * (1 - self.res)
+
+    def __call__(self, x):
+        return self.forward(x)
 
 
 class Tanh:
     # DO NOT DELETE
-	def __init__(self):
-		pass
-	def forward(self, x):
-		self.res = np.tanh(x)
-		return self.res
-	def backward(self):
-		return 1 - (self.res**2)
-	def __call__(self, x):
-		return self.forward(x)
+    def __init__(self):
+        pass
+
+    def forward(self, x):
+        self.res = np.tanh(x)
+        return self.res
+
+    def backward(self):
+        return 1 - (self.res ** 2)
+
+    def __call__(self, x):
+        return self.forward(x)
+
 
 class Linear():
     # DO NOT DELETE
@@ -35,7 +43,7 @@ class Linear():
 
         self.W = np.random.randn(out_feature, in_feature)
         self.b = np.zeros(out_feature)
-        
+
         self.dW = np.zeros(self.W.shape)
         self.db = np.zeros(self.b.shape)
 
@@ -53,61 +61,68 @@ class Linear():
         dx = np.dot(delta, self.W.T)
         return dx
 
+
 class GRU_Cell:
-	"""docstring for GRU_Cell"""
-	def __init__(self, in_dim, hidden_dim):
-		self.d = in_dim
-		self.h = hidden_dim
-		h = self.h
-		d = self.d
-		self.x_t=0
+    """docstring for GRU_Cell"""
 
-		self.Wzh = np.random.randn(h,h)
-		self.Wrh = np.random.randn(h,h)
-		self.Wh  = np.random.randn(h,h)
+    def __init__(self, in_dim, hidden_dim):
+        self.d = in_dim
+        self.h = hidden_dim
+        h = self.h
+        d = self.d
+        self.x_t = 0
 
-		self.Wzx = np.random.randn(h,d)
-		self.Wrx = np.random.randn(h,d)
-		self.Wx  = np.random.randn(h,d)
+        self.Wzh = np.random.randn(h, h)
+        self.Wrh = np.random.randn(h, h)
+        self.Wh = np.random.randn(h, h)
 
-		self.dWzh = np.zeros((h,h))
-		self.dWrh = np.zeros((h,h))
-		self.dWh  = np.zeros((h,h))
+        self.Wzx = np.random.randn(h, d)
+        self.Wrx = np.random.randn(h, d)
+        self.Wx = np.random.randn(h, d)
 
-		self.dWzx = np.zeros((h,d))
-		self.dWrx = np.zeros((h,d))
-		self.dWx  = np.zeros((h,d))
+        self.dWzh = np.zeros((h, h))
+        self.dWrh = np.zeros((h, h))
+        self.dWh = np.zeros((h, h))
 
-		self.z_act = Sigmoid()
-		self.r_act = Sigmoid()
-		self.h_act = Tanh()
+        self.dWzx = np.zeros((h, d))
+        self.dWrx = np.zeros((h, d))
+        self.dWx = np.zeros((h, d))
+
+        self.z_act = Sigmoid()
+        self.r_act = Sigmoid()
+        self.h_act = Tanh()
 
         # Define other variables to store forward results for backward here
+        self.z_t = None
+        self.r_t = None
+        self.h_tilde_t = None
+        self.h_t = None
 
+    def init_weights(self, Wzh, Wrh, Wh, Wzx, Wrx, Wx):
+        self.Wzh = Wzh
+        self.Wrh = Wrh
+        self.Wh = Wh
+        self.Wzx = Wzx
+        self.Wrx = Wrx
+        self.Wx = Wx
 
-	def init_weights(self, Wzh, Wrh, Wh, Wzx, Wrx, Wx):
-		self.Wzh = Wzh
-		self.Wrh = Wrh
-		self.Wh = Wh
-		self.Wzx = Wzx
-		self.Wrx = Wrx
-		self.Wx  = Wx
+    def __call__(self, x, h):
+        return self.forward(x, h)
 
-	def __call__(self, x, h):
-		return self.forward(x,h)
+    def forward(self, x, h):
+        # input:
+        # 	- x: shape(input dim),  observation at current time-step
+        # 	- h: shape(hidden dim), hidden-state at previous time-step
+        #
+        # output:
+        # 	- h_t: hidden state at current time-step
 
-	def forward(self, x, h):
-		# input:
-		# 	- x: shape(input dim),  observation at current time-step
-		# 	- h: shape(hidden dim), hidden-state at previous time-step
-		# 
-		# output:
-		# 	- h_t: hidden state at current time-step
-        raise NotImplementedError
+        return 5
 
+    	#raise NotImplementedError
 
     # This  must calculate the gradients wrt the parameters and returns the derivative wrt the inputs, xt and ht, to the cell.
-	def backward(self, delta):
+    def backward(self, delta):
         # input:
         #  - delta:  shape (hidden dim), summation of derivative wrt loss from next layer at
         #            the same time-step and derivative wrt loss from same layer at
@@ -115,9 +130,10 @@ class GRU_Cell:
         # output:
         #  - dx: Derivative of loss wrt the input x
         #  - dh: Derivative  of loss wrt the input hidden h
-	    raise NotImplementedError
+        raise NotImplementedError
 
-# This is the neural net that will run one timestep of the input 
+
+# This is the neural net that will run one timestep of the input
 # You only need to implement the forward method of this class. 
 # This is to test that your GRU Cell implementation is correct when used as a GRU.	
 class CharacterPredictor(object):
@@ -127,16 +143,17 @@ class CharacterPredictor(object):
 
     def init_rnn_weights(self, w_hi, w_hr, w_hn, w_ii, w_ir, w_in):
         # DO NOT MODIFY
-        self.rnn.init_weights(w_hi, w_hr, w_hn, w_ii, w_ir, w_in) 
+        self.rnn.init_weights(w_hi, w_hr, w_hn, w_ii, w_ir, w_in)
 
     def __call__(self, x, h):
-        return self.forward(x, h)        
+        return self.forward(x, h)
 
     def forward(self, x, h):
         # A pass through one time step of the input 
         raise NotImplementedError
 
-# An instance of the class defined above runs through a sequence of inputs to generate the logits for all the timesteps. 
+
+# An instance of the class defined above runs through a sequence of inputs to generate the logits for all the timesteps.
 def inference(net, inputs):
     # input:
     #  - net: An instance of CharacterPredictor
