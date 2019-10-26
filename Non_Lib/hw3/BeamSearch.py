079a633c94ab7de4956da71be49f79d06855f687
ghostian <junxiao.guo@outlook.com>
2019-10-25 23:01:40 -0400

    HW3P1 90

--- Non_Lib/hw3/BeamSearch.py
+++ Non_Lib/hw3/BeamSearch.py
@@ -5,14 +5,13 @@ import sys
 def GreedySearch(SymbolSets, y_probs):
     '''
     SymbolSets: This is the list containing all the symbols i.e. vocabulary (without blank)
-    
+
     y_probs: Numpy array of (# of symbols+1,Seq_length,batch_size). Note that your
     batch size for part 1 would always remain 1, but if you plan to use
     your implementation for part 2 you need to incorporate batch_size.
     Return the forward probability of greedy path and corresponding compressed symbol
     sequence i.e. without blanks and repeated symbols.
     '''
-    # print(y_probs)
     best_path = ""
     score = np.array([1.])
     seq_len = len(y_probs[0])
@@ -33,36 +32,34 @@ def prune(PathWithTerminalBlank, PathWithTerminalSymbol, BlankPathScore, PathSco
     PrunedPathScore = {}
     PrunedPathWithTerminalBlank = set()
     PrunedPathWithTerminalSymbol = set()
-    scorelist = set()
-    for p in PathWithTerminalBlank:
-        scorelist.add(BlankPathScore[p])
-    for p in PathWithTerminalSymbol:
-        scorelist.add(PathScore[p])
+    scorelist = []
+    for p in BlankPathScore.keys():
+        scorelist.append(BlankPathScore[p])
+    for p in PathScore.keys():
+        scorelist.append(PathScore[p])
     scorelist = sorted(scorelist)
     cutoff = scorelist[-BeamWidth]
-    for p in PathWithTerminalBlank:
+    for p in BlankPathScore.keys():
         if BlankPathScore[p] >= cutoff:
             PrunedPathWithTerminalBlank.add(str(p))
             PrunedBlankPathScore[p] = BlankPathScore[p]
-    for p in PathWithTerminalSymbol:
+    for p in PathScore.keys():
         if PathScore[p] >= cutoff:
             PrunedPathWithTerminalSymbol.add(str(p))
             PrunedPathScore[p] = PathScore[p]
-    # print(PrunedPathWithTerminalBlank)
-    # print(PrunedPathWithTerminalSymbol)
+    # print(PrunedPathScore,PrunedBlankPathScore)
+    # print(PrunedPathWithTerminalBlank,PrunedPathWithTerminalSymbol)
+    # sys.exit(1)
     return PrunedPathWithTerminalBlank, PrunedPathWithTerminalSymbol, PrunedPathScore, PrunedBlankPathScore
 
 
 def Blankextend(PathWithTerminalBlank, PathWithTerminalSymbol, y, PathScore, BlankPathScore, SymbolSets):
     blankpathupdate = set()
     blankscoreupdate = {}
-    # print(PathWithTerminalBlank,PathWithTerminalSymbol)
-    # sys.exit(1)
-    for p in PathWithTerminalBlank:
-        # print(p)
+    for p in BlankPathScore.keys():
         blankpathupdate.add(p)
         blankscoreupdate[p] = (BlankPathScore[p] * y[0])
-    for p in PathWithTerminalSymbol:
+    for p in PathScore.keys():
         if p in blankpathupdate:
             blankscoreupdate[p] += PathScore[p] * y[0]
         else:
@@ -76,7 +73,7 @@ def Symbolextend(PathWithTerminalBlank, PathWithTerminalSymbol, y, PathScore, Bl
     scoreupdate = {}
     newpath = ""
 
-    for p in PathWithTerminalSymbol:
+    for p in PathScore.keys():
         for c in range(len(symbolset)):
             if symbolset[c] == p[-1]:
                 newpath = p
@@ -84,7 +81,7 @@ def Symbolextend(PathWithTerminalBlank, PathWithTerminalSymbol, y, PathScore, Bl
                 newpath = p + symbolset[c]
             pathupdate.add(newpath)
             scoreupdate[newpath] = PathScore[p] * y[c][0]
-    for p in PathWithTerminalBlank:
+    for p in BlankPathScore.keys():
         for c in range(len(symbolset)):
             newpath = p + symbolset[c]
             if newpath in pathupdate:
@@ -112,7 +109,7 @@ def mergeIdentical(PathWithTerminalBlank, PathWithTerminalSymbol, BlankPathScore
 def BeamSearch(SymbolSets, y_probs, BeamWidth):
     '''
     SymbolSets: This is the list containing all the symbols i.e. vocabulary (without blank)
-    
+
     y_probs: Numpy array of (# of symbols+1,Seq_length,batch_size). Note that your
     batch size for part 1 would always remain 1, but if you plan to use
     your implementation for part 2 you need to incorporate batch_size.
@@ -125,7 +122,6 @@ def BeamSearch(SymbolSets, y_probs, BeamWidth):
 
     #################################################
     def PathInit(Symbolsets, y_probs, BeamWitdth, PathScore, BlankPathScore):
-        # print(BeamWidth)
         path = ""
         BlankPathScore[path] = y_probs[0][0][0]
         InitialPathsWithFinalBlank = set(path)
@@ -137,12 +133,10 @@ def BeamSearch(SymbolSets, y_probs, BeamWidth):
         return prune(InitialPathsWithFinalBlank, InitialPathsWithFinalSymbol, BlankPathScore, PathScore, BeamWidth)
 
     PathWithTerminalBlank, PathWithTerminalSymbol, PathScore, BlankPathScore = PathInit(SymbolSets, y_probs, BeamWidth, PathScore, BlankPathScore)
-    # print("{}\n{}".format(PathWithTerminalBlank, PathWithTerminalSymbol))
-    # print("{}\n{}".format(PathScore, BlankPathScore))
+    print(PathWithTerminalBlank, BlankPathScore)
     for i in range(1, seq_len):
         blankupdate, blankscoreupdate = Blankextend(PathWithTerminalBlank, PathWithTerminalSymbol, y=y_probs[0, i], PathScore=PathScore, BlankPathScore=BlankPathScore, SymbolSets=SymbolSets)
         symbolupdate, symbolscoreupdate = Symbolextend(PathWithTerminalBlank, PathWithTerminalSymbol, y=y_probs[1:, i], PathScore=PathScore, BlankPathScore=BlankPathScore, symbolset=SymbolSets)
-        # print(blankupdate, symbolupdate)
         PathWithTerminalBlank, PathWithTerminalSymbol, PathScore, BlankPathScore = prune(blankupdate, symbolupdate, blankscoreupdate, symbolscoreupdate, BeamWidth)
     MergedPaths, FinalPathScore = mergeIdentical(PathWithTerminalBlank, PathWithTerminalSymbol, BlankPathScore, PathScore)
     BestPath, BestScore = max(FinalPathScore.items(), key=lambda k: k[1])
@@ -159,6 +153,3 @@ if __name__ == "__main__":
     # best_path, score = GreedySearch(SymbolSets, y_probs)
     BeamWidth = 3
     BestPath, BestScore = BeamSearch(SymbolSets, y_probs, BeamWidth)
-    # print(BestPath,BestScore)
-    # BestPath, MergedPathScores = BeamSearch(SymbolSets, y_probs, BeamWidth)
-    # print(BestPath, MergedPathScores)
