918016b823db3322113be49699c7cbe868ae21ba
ghostian <junxiao.guo@outlook.com>
2019-11-05 11:37:28 -0500

    HW3P2 Update (Criterion worked)

--- kaggle/rnn/net.py
+++ kaggle/rnn/net.py
@@ -70,7 +70,7 @@ def train_epoch_packed(model, optimizer, train_loader, val_loader, inputs_len):
     for inputs, targets in val_loader:
         nwords += np.sum(np.array([len(l) for l in inputs]))
         batch_id += 1
-        outputs, outlens = model(inputs, inputs_len)
+        outputs, outlens = model(inputs, new_inputlen)
         cur_Y = Y[(batch_id - 1) * BATCH_SIZE:batch_id * BATCH_SIZE]
         cur_Y_len = Y_lens[(batch_id - 1) * BATCH_SIZE:batch_id * BATCH_SIZE]
         cur_Y = torch.nn.utils.rnn.pad_sequence(cur_Y).T
