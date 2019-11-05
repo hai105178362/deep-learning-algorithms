2519e18ff86243abf36a335cbd6680fc7c6f7872
ghostian <junxiao.guo@outlook.com>
2019-11-05 18:06:34 -0500

    HW3P2 Update (Criterion worked)

--- kaggle/rnn/net.py
+++ kaggle/rnn/net.py
@@ -18,7 +18,7 @@ BATCH_SIZE = 64
 class Model(torch.nn.Module):
     def __init__(self, in_vocab, out_vocab, hidden_size):
         super(Model, self).__init__()
-        self.lstm = torch.nn.LSTM(in_vocab, hidden_size, bidirectional=True, num_layers=2,dropout=0.2)
+        self.lstm = torch.nn.LSTM(in_vocab, hidden_size, bidirectional=True, num_layers=2,dropout=0.5)
         # self.lstm = torch.nn.LSTM(in_vocab, hidden_size, bidirectional=True)
         self.output = torch.nn.Linear(hidden_size * 2, out_vocab)
 
@@ -151,8 +151,8 @@ if __name__ == "__main__":
 
     train_loader = DataLoader(X, shuffle=False, batch_size=BATCH_SIZE, collate_fn=collate_lines)
     val_loader = DataLoader(valX, shuffle=False, batch_size=BATCH_SIZE, collate_fn=collate_lines)
-    model = Model(in_vocab=40, out_vocab=47, hidden_size=128)
+    model = Model(in_vocab=40, out_vocab=47, hidden_size=256)
     optimizer = torch.optim.Adam(model.parameters(), lr=0.001, weight_decay=1e-6)
-    for i in range(150):
+    for i in range(1000):
         print("==========Epoch {}==========".format(i + 1))
         train_epoch_packed(model, optimizer, train_loader, val_loader, inputs_len=X_lens, val_inputs_len=valX_lens, n_epoch=i)
