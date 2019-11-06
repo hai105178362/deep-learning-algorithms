1bc962144661ad3688d79be5bf918c889c6fe2c5
ghostian <junxiao.guo@outlook.com>
2019-11-05 23:41:07 -0500

    HW3P2 Dev

--- kaggle/rnn/net.py
+++ kaggle/rnn/net.py
@@ -12,13 +12,13 @@ from torch.autograd import Variable
 
 
 DEVICE = "cuda" if torch.cuda.is_available() else "cpu"
-BATCH_SIZE = 32
+BATCH_SIZE = 16
 
 
 class Model(torch.nn.Module):
     def __init__(self, in_vocab, out_vocab, hidden_size):
         super(Model, self).__init__()
-        self.lstm = torch.nn.LSTM(in_vocab, hidden_size, bidirectional=True, num_layers=3,dropout=0.15)
+        self.lstm = torch.nn.LSTM(in_vocab, hidden_size, bidirectional=True, num_layers=3)
         # self.lstm = torch.nn.LSTM(in_vocab, hidden_size, bidirectional=True)
         self.output = torch.nn.Linear(hidden_size*2, out_vocab)
 
