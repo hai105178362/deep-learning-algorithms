e1e90c55ab2eb5918db391800ff33c84e127ccac
ghostian <junxiao.guo@outlook.com>
2019-11-07 10:22:52 -0500

    HW3P2 Dev

--- kaggle/rnn/baseline.py
+++ kaggle/rnn/baseline.py
@@ -126,7 +126,7 @@ if __name__ == "__main__":
     valypath = "dataset.nosync/HW3P2_Data/wsj0_dev_merged_labels.npy"
     trainxpath = "dataset.nosync/HW3P2_Data/wsj0_train.npy"
     trainypath = "dataset.nosync/HW3P2_Data/wsj0_train_merged_labels.npy"
-    task = "train"
+    task = "va"
     if task == "train":
         xpath = trainxpath
         ypath = trainypath
