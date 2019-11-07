1532b62fe8938e9ae850b7de64b97a22c748f71d
ghostian <junxiao.guo@outlook.com>
2019-11-07 10:50:54 -0500

    HW3P2 Dev

--- kaggle/rnn/netdev.py
+++ kaggle/rnn/netdev.py
@@ -128,7 +128,7 @@ if __name__ == "__main__":
     valypath = "dataset.nosync/HW3P2_Data/wsj0_dev_merged_labels.npy"
     trainxpath = "dataset.nosync/HW3P2_Data/wsj0_train.npy"
     trainypath = "dataset.nosync/HW3P2_Data/wsj0_train_merged_labels.npy"
-    task = "train"
+    task = "d"
     if task == "train":
         xpath = trainxpath
         ypath = trainypath
