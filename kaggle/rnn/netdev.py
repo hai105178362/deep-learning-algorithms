a1941e3e06ccc35298aea685d25cf744f58dbda1
ghostian <junxiao.guo@outlook.com>
2019-11-07 12:45:27 -0500

    HW3P2 Dev

--- kaggle/rnn/netdev.py
+++ kaggle/rnn/netdev.py
@@ -23,7 +23,7 @@ class Model(torch.nn.Module):
     def __init__(self, in_vocab, out_vocab, hidden_size):
         super(Model, self).__init__()
         # self.lstm = torch.nn.LSTM(in_vocab, hidden_size, bidirectional=True, num_layers=3)
-        self.lstm = torch.nn.LSTM(in_vocab, hidden_size, bidirectional=True, num_layers=4,dropout=0.3).to(DEVICE)
+        self.lstm = torch.nn.LSTM(in_vocab, hidden_size, bidirectional=True, num_layers=4).to(DEVICE)
         # self.lstm = torch.nn.LSTM(in_vocab, hidden_size, bidirectional=True)
         self.lf = torch.nn.Linear(out_vocab,out_vocab).to(DEVICE)
         ###
