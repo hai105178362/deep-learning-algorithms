cad81b0dd4935b5d5dee266e1f250ee7882a104e
ghostian <junxiao.guo@outlook.com>
2019-11-08 10:25:48 -0500

    HW3P2 Dev

--- kaggle/rnn/net5layers.py
+++ kaggle/rnn/net5layers.py
@@ -13,7 +13,7 @@ from torch.autograd import Variable
 
 
 DEVICE = "cuda" if torch.cuda.is_available() else "cpu"
-BATCH_SIZE = 48
+BATCH_SIZE = 32
 HIDDEN_SIZE = 512
 # HIDDEN_SIZE = 16
 # HIDDEN_SIZE = 128
